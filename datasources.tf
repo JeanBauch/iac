ffe15b1828528977be04e5cb524930d371aa0fe3
Jean Bauch <jean.bauch06@gmail.com>
2025-03-20 22:45:49 -0300

    feat: variable

--- datasources.tf
+++ datasources.tf
@@ -1,3 +1,3 @@
 data "aws_s3_bucket" "s3_bucket" {
-    bucket = "bucket-iac-jb-${terraform.workspace}"
+    bucket = "${var.org_name}-${terraform.workspace}"
 }
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket" {
-   bucket = "bucket-iac-jb-${terraform.workspace}"
+   bucket = "${var.org_name}-${terraform.workspace}"
  
    tags = {
      Name    = "Primeiro bucket"
