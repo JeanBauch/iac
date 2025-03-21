90975af31ce97fd758881219f2f19e1f0d0d5dd6
Jean Bauch <jean.bauch06@gmail.com>
2025-03-20 22:22:05 -0300

    workspaces

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "s3_bucket" {
-   bucket = "bucket-iac-jb"
+   bucket = "bucket-iac-jb-${terraform.workspace}"
  
    tags = {
      Name    = "Primeiro bucket"
      Iac     = "true"
+     context = "${terraform.workspace}"
    }
  }
